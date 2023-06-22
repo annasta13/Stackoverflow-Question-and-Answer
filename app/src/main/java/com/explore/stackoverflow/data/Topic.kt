e65096e43b10e79072919f94ecf0bf8d8b7dae0a
Annas Surdyanto <annas.surdyanto@gmail.com>
2023-06-23 01:29:59 +0700

    Listing Topics

--- app/src/main/java/com/explore/stackoverflow/data/Topic.kt
+++ app/src/main/java/com/explore/stackoverflow/data/Topic.kt
@@ -2,6 +2,7 @@ package com.explore.stackoverflow.data
 
 import androidx.fragment.app.Fragment
 import com.explore.stackoverflow.ui.answer.question76433128.SampleFragment
+import com.explore.stackoverflow.ui.answer.question76445761.Sample2Fragment
 import com.explore.stackoverflow.ui.answer.question76480901.Sample3Fragment
 
 data class Topic(
@@ -18,6 +19,11 @@ data class Topic(
             ),
             Topic(
                 2,
+                Sample2Fragment(),
+                "Android two way databinding with Double and optional radix point causing some trouble"
+            ),
+            Topic(
+                3,
                 Sample3Fragment(),
                 "Facing issue While making dynamic views using Relative layout"
             )
